9148ee21e80fca199b89056999b0c9ce2c2b4050
Quentin Pugeat <contact@quentinpugeat.fr>
2025-04-01 23:56:54 +0200

    Afficher la version correcte de l'Assembly dans l'onglet À propos de, corriger la version du Package

--- Square Note/MainWindowHelpPage.xaml.cs
+++ Square Note/MainWindowHelpPage.xaml.cs
@@ -1,6 +1,7 @@
 using Microsoft.UI.Xaml.Controls;
 using Square_Note.Services;
 using System.Diagnostics;
+using System.Reflection;
 
 namespace Square_Note
 {
@@ -11,6 +12,8 @@ namespace Square_Note
             InitializeComponent();
             MajesticielsUpdater.Instance.UpdateAvailable += MajesticielsUpdater_UpdateAvailable;
             MajesticielsUpdater.Instance.CheckUpdatesNow();
+
+            VersionNumberTextBlock.Text = Assembly.GetExecutingAssembly().GetName().Version?.ToString();
         }
 
         private void MajesticielsUpdater_UpdateAvailable(object sender, MajesticielsUpdate e)
